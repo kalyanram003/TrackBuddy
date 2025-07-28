6405723ebdbe8e4ada819dbf0feba8f0f3a06625
kalyanram003 <kalyanram2053@gmail.com>
2025-07-28 20:09:32 +0530

    Project initial

---
+++ src/main/java/com/kalyan/RemoteWorkTracker/RemoteWorkTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kalyan.RemoteWorkTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteWorkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteWorkTrackerApplication.class, args);
+	}
+
+}
